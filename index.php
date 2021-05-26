a7c7cbc77c65e7817a4b263375fe9f6b9f0a741b
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-05-26 14:57:08 +0200

    Index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <title>Locker</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="text">TRY TO OPEN THE SAFE!</div>
+        <div class="page">
+            <div class="title">
+                <img src="images/closed-locker.svg">
+
+            </div>
+
+            <form action="#" method="POST">
+                <label>Enter the code</label>
+                <input type="password" id="code" name="code" placeholder="Code">
+                <button type="submit" name="Open Locker" id="btnSubmit">CHECK CODE</button>
+
+            </form>
+
+</body>
+
+</html>
